c37b8b71eec23b18bb83a0ec8330741c447fc508
FayronWDG <fayronwdg@gmail.com>
2025-04-25 23:28:44 +0000

    antidante

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+$("document").ready(function(){
+    $("#game1").hide();
+    $("#game2").hide();
+    $("#game3").hide();
+
+
+
+
+    //jogo 1
+    $("#btgame1").click(function(){
+    
+    
+    
+        $("#selecao_games").hide();
+        $("#game1").show();
+        $("#game2").hide();
+        $("#game3").hide();
+        
+
+        $("#flw1").click(){
+            $("#msg").text("DANTE VIRGEM BE LIKE");
+        };
+        
+        $("#flw2").click(){
+            $("#msg").text("VIRGILMENTE VIRGEM BE LIKE");
+        };
+    
+        $("#flw3").click(){
+            $("#msg").text(" VIRGEM BE LIKE");
+        };
+    
+        $("#flw4").click(){
+            $("#msg").text("DANTE VIRGEM BE LIKE");
+        };
+    
+    
+    });
+
+
+
+
+});
